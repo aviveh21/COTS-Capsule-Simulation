b66351d65b5e3714b116e24f1c7db657fd31d963
Alex Segal <segalale@gmail.com>
2023-04-08 15:10:40 +0300

    Sync scripts

--- run_instance.sh
+++ run_instance.sh
@@ -2,5 +2,5 @@
 
 # This script runs a geant4 instance and sends a config file as user data. Then, the instance should start the simulation automatically
 
-aws ec2 run-instances --image-id ami-0453284ac1938eee9 --count 1 --instance-type t2.micro --key-name alex_key --security-group-ids sg-0b3decb39d029facf --user-data file://init_geant_config_script.txt
+aws ec2 run-instances --image-id ami-0e30bf3c72732e4a3 --count 1 --instance-type t2.micro --key-name alex_key --security-group-ids sg-0b3decb39d029facf --user-data file://init_geant_config_script.txt
 
