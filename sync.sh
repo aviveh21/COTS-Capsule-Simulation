b3e9b9c2cc5c9c9b0ab97caf20726156b1af3850
Alex Segal <segalale@gmail.com>
2023-06-23 02:26:10 +0300

    New ami (V19)

--- sync.sh
+++ sync.sh
@@ -24,6 +24,13 @@ RSYNC_OPTS="-avz --no-perms --no-owner --progress --no-group $DRY_RUN"
 echo "Syncing local directories (no /etc)"
 rsync $RSYNC_OPTS --exclude-from="$SCRIPT_DIR"/exclude_sync -e "ssh -i $SSH_KEY" $SCRIPT_DIR/ ubuntu@$ip:~/COTS-Capsule-Simulation/ --delete-after
 
+echo "Syncing code (Full_detecor) .cc files"
+rsync $RSYNC_OPTS  --include '*/' --include '*.cc' --exclude '*' -e "ssh -i $SSH_KEY" $SCRIPT_DIR/Full_Detector ubuntu@$ip:~/COTS-Capsule-Simulation/
+
+echo "Syncing code (Full_detecor) .hh files"
+rsync $RSYNC_OPTS  --include '*/' --include '*.hh' --exclude '*' -e "ssh -i $SSH_KEY" $SCRIPT_DIR/Full_Detector ubuntu@$ip:~/COTS-Capsule-Simulation/
+
+
 echo "Syncing /etc" 
 rsync $RSYNC_OPTS -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" $SCRIPT_DIR/etc/ ubuntu@$ip:/etc/
 
